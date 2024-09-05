0cfb39108844ab8952e6c29805d0899c28f328ea
650elx <bprokop02@gmail.com>
2024-09-05 19:12:17 +0200

    basic client example

---
+++ templates/client/app.py
@@ -0,0 +1,9 @@
+import os
+from sinch import SinchClient
+
+
+sinch_client = SinchClient(
+    key_id=os.getenv("KEY_ID"),
+    key_secret=os.getenv("KEY_SECRET"),
+    project_id=os.getenv("PROJECT_ID")
+)
